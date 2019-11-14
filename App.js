ef7a62153eb6509afa78281bebc6d080d066a72c
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-11-14 13:19:49 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
